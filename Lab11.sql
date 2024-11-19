f1c886bb67c2fcbebbbcbfacbb73bbab15d39c9d
Clein Ecoy <clein.ecoy@gmail.com>
2024-11-19 15:22:45 -0500

    Lab11.sql

--- Lab11.sql
+++ Lab11.sql
@@ -1,29 +1,29 @@
  ## "SQL Exercises" 
   
- 1. "Assigments for a specific Course"  
+  1. "Assigments for a specific Course"  
   SELECT title, due_date
   FROM assignments 
   WHERE course_id LIKE 'COMP1234'
 
-- Exercise 2. Earliest Assignments Due Date
+  2. "Earliest Assignments Due Date"
   SELECT min(due_date)
   FROM assignments
 
-- Exercise 3. Latest Assignment Due date
+  3. "Latest Assignment Due date"
   SELECT max(due_date)
   FROM assignments
 
--Exercise 4. Assignemtns due on a Specific date 
+  4. "Assignemtns due on a Specific date"
   SELECT title, due_date
   FROM assignments 
   WHERE due_date = '2024-10-08'
 
--Exercise 5. Find all assignemnts due in October
+  5. "Find all assignemnts due in October"
   SELECT title, due_date
   FROM assignements 
   WHERE due_date LIKE '2024-10%'
 
--Exercise 6. Find the most recent completed assignment
+  6. "Find the most recent completed assignment"
   SELECT *
   FROM assignments 
   WHERE status = 'Completed'
