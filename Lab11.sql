81b1da6e81b3e5123bf73b8983f65a66d8af41e1
Clein Ecoy <clein.ecoy@gmail.com>
2024-11-19 15:12:00 -0500

    Lab11.sql

--- Lab11.sql
+++ Lab11.sql
@@ -1,2 +1,31 @@
-# SQL exercises 
-Exercise 1. Asignemtns for a specific Course  
+# SQL Exercises 
+  
+- Exercise 1. Asignemtns for a specific Course  
+  SELECT title, due_date
+  FROM assignments 
+  WHERE course_id LIKE 'COMP1234'
+
+- Exercise 2. Earliest Assignments DUe Date
+  SELECT min(due_date)
+  FROM assignments
+
+- Exercise 3. Latest Assignment Due date
+  SELECT   max(due_date)
+  FROM   assignments
+
+-Exercise 4. Assignemtns due on a Specific date 
+  SELECT title, due_date
+  FROM assignments 
+  WHERE due_date = '2024-10-08'
+
+-Exercise 5. Find all assignemnts due in October
+  SELECT title, due_date
+  FROM assignements 
+  WHERE due_date LIKE '2024-10%'
+
+-Exercise 6. Find the most recent completed assignment
+  SELECT *
+  FROM assignments 
+  WHERE status = 'Completed'
+  AND   due_date < '2024-1'
+ORDER BY due_date;
